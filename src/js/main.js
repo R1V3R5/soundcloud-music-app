4ab58d496b9ffb8a816e008a7fe2aa743831fd17
Joshua Rivers <joshua.rivers@gmail.com>
2016-07-01 15:08:33 -0400

    minor tweaks to js and css

--- src/js/main.js
+++ src/js/main.js
@@ -7,29 +7,29 @@ var clientID = 'client_id=2a333b1aa27e6f8d8d042869169ddf94';
 
 function resultTmpl(data) {
   if (data.artwork_url === null) {
-    data.artwork_url = "http://u.sera.to/users/40/306540/avatar_mid_1332880124.jpeg"
+    data.artwork_url = "https://pbs.twimg.com/profile_images/2577267058/avatar.jpg"
   }
   var template = `
 		<div class="result-grid">
 			<div class="avatar">
-				<img src="${data.artwork_url}">
+				<a href="${data.stream_url}?${clientID}" class="play-me" title="${data.title}"><img src="${data.artwork_url}"></a>
 			</div>
 			<div class="title">
-				<a href="${data.stream_url}?${clientID}" class="play-me"><h5>${data.title}</h5></a>
+				<a href="${data.stream_url}?${clientID}" class="play-me" title="${data.title}"><h5>${data.title}</h5></a>
 			</div>
 			<div class="band">
 				<h5>${data.user.username}</h5>
 			</div>
 		</div>`
-  console.log(template)
+    // console.log(template)
   return template;
 };
 
 
-function streamTmpl(data) {
+function streamTmpl(data, title) {
   var audioTmpl = `
   	<audio src="${data}" autobuffer autoloop loop controls autoplay></audio>
-  `
+  	<h2 class="nowPlaying">Now playing: ${title}</h2>`
   $('.now-playing').html(audioTmpl)
 };
 
@@ -37,7 +37,8 @@ function streamTmpl(data) {
 function clickMe() {
   $('.play-me').on('click', function(event) {
     event.preventDefault();
-    streamTmpl(event.delegateTarget.href);
+    console.log(event)
+    streamTmpl(event.delegateTarget.href, event.delegateTarget.title);
   })
 };
 var resultsHTML;
@@ -51,8 +52,9 @@ $('.search-button').on('click', function(event) {
   }).then(function(response) {
     console.log(response);
     resultsHTML = response.map(resultTmpl)
-    console.log(resultsHTML)
+    // console.log(resultsHTML)
     $('.search-results').html(resultsHTML)
+    $('.now-playing').empty()
     clickMe();
   })
 
